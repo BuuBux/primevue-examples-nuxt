2821a73146e74bf8942ecf58be54403023a8b166
Kamil Kras <buubux@gmail.com>
2023-04-19 15:30:31 +0000

    Update 4 files

--- app.vue
+++ app.vue
@@ -1,36 +1,40 @@
 <template>
-	<div class="container">
-		<div class="flex flex-column align-items-center mt-1">
-			<img alt="Vue logo" src="./assets/primevue-logo.png" />
-			<PrimeToast />
+  <div class="container">
+    <div class="flex flex-column align-items-center mt-1">
+      <img alt="Vue logo" src="./assets/primevue-logo.png" />
+      <PrimeToast />
+      <Avatar
+        image="https://www.imgworlds.com/wp-content/uploads/2015/12/18-CONTACTUS-HEADER.jpg"
+      />
 
-			<div class="mt-4">
-				<form @submit.prevent="greet">
-					<PrimeInputText type="text" v-model="text" class="mr-3" />
-					<PrimeButton type="submit" label="Submit" />
-				</form>
-			</div>
-		</div>
-	</div>
+      <div class="mt-4">
+        <form @submit.prevent="greet">
+          <PrimeInputText type="text" v-model="text" class="mr-3" />
+          <PrimeButton type="submit" label="Submit" />
+        </form>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
 import { useToast } from "primevue/usetoast";
+import Avatar from "primevue/avatar";
 
-const text = ref('');
+const text = ref("");
 const toast = useToast();
 const greet = () => {
-	toast.add({ severity: "info", summary: "Hello " + text.value });
+  toast.add({ severity: "info", summary: "Hello " + text.value });
 };
 </script>
 
 <style>
 .container {
-	margin: 0 auto;
-	min-height: 100vh;
-	display: flex;
-	justify-content: center;
-	align-items: center;
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
